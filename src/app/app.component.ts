e5cd505ea03d5e41fd58708d2f3285fdbf65b96f
javiair <javiair001@gmail.com>
2019-04-02 14:17:46 -0400

    App optimaizations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  loadedFeature = 'recipe';
-
-  onNavigate(eventData: string) {
-    this.loadedFeature = eventData;
-  }
 }
