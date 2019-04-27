782465a5ff98696d0b41cde94e23b836cc4ae171
javiair <javiair001@gmail.com>
2019-04-27 13:39:07 -0400

    Replaced EventEmitter with an Observable

--- src/app/Shared/Services/shopping-list.service.ts
+++ src/app/Shared/Services/shopping-list.service.ts
@@ -1,12 +1,13 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
-  ingredientsUpdated = new EventEmitter<Ingredient[]>();
+  ingredientsUpdated = new Subject<Ingredient[]>();
 
   constructor() { }
 
@@ -21,12 +22,12 @@ export class ShoppingListService {
 
   newIngredient(eventData: Ingredient) {
     this.ingredients.push(eventData);
-    this.ingredientsUpdated.emit(this.ingredients.slice());
+    this.ingredientsUpdated.next(this.ingredients.slice());
   }
 
   recipeToShoppingList(ingredient: Ingredient[]) {
     this.ingredients.push(...ingredient);
-    this.ingredientsUpdated.emit(this.ingredients.slice());
+    this.ingredientsUpdated.next(this.ingredients.slice());
   }
 
 }
