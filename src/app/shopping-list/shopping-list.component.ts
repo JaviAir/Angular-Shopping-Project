7cb7149323573ed043add7d8f7c1026a5a4a6f60
javiair <javiair001@gmail.com>
2019-04-27 13:37:43 -0400

    Manually Unsubscribe from custom observable

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../Shared/ingredient.model';
 import { ShoppingListService } from '../Shared/Services/shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private shopListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shopListService.getIngredients();
-    this.shopListService.ingredientsUpdated.subscribe(
+    this.subscription = this.shopListService.ingredientsUpdated.subscribe(
       (ingredients: Ingredient[]) => { this.ingredients = ingredients; }
     );
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
